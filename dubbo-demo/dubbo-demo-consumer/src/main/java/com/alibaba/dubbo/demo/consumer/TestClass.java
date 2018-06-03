427e74a0b4e2d465e083d00de14a98074e40ec7d
朱彦霖 <xyxzyl@zhuyanlindeMacBook-Pro.local>
2018-06-03 22:16:58 +0800

    dubbo-demo

---
+++ dubbo-demo/dubbo-demo-consumer/src/main/java/com/alibaba/dubbo/demo/consumer/TestClass.java
@@ -0,0 +1,8 @@
+package com.alibaba.dubbo.demo.consumer;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("user.home"));
+    }
+}
